b3dc679a0c3ba629b013cd59a499d29da8400714
quisiou <marco.casteleiro@gmail.com>
2026-09-24 15:18:29 +0200

    added gh-dash to git config in nvim

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,11 +1,47 @@
 -- nvim/lua/plugins/lazygit.lua
 
 
+local function open_floating_terminal(cmd)
+    local width = math.floor(vim.o.columns * 0.9)
+    local height = math.floor(vim.o.lines * 0.9)
+    local row = math.floor((vim.o.lines - height) / 2)
+    local col = math.floor((vim.o.columns - width) / 2)
+
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        width = width,
+        height = height,
+        row = row,
+        col = col,
+        style = "minimal",
+        border = "rounded",
+    })
+
+    vim.fn.termopen(cmd, {
+        on_exit = function()
+            if vim.api.nvim_win_is_valid(win) then
+                vim.api.nvim_win_close(win, true)
+            end
+        end,
+    })
+
+    vim.cmd("startinsert")
+end
+
 return {
     "kdheepak/lazygit.nvim",
     cmd = "LazyGit",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
         { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        {
+            "<leader>gd",
+            function()
+                open_floating_terminal("gh dash")
+            end,
+            desc = "Open gh-dash",
+        },
     },
 }
